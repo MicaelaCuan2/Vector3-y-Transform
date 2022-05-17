39ad096a31870e8205e1fff980bec942932cafb9
MicaelaCuan2 <micucuan@gmail.com>
2022-05-17 12:10:03 -0300

    Clase terminada

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Vector3 movement;
+
+    void Update()
+    {
+        if (transform.eulerAngles.y < 160)
+        {
+            transform.eulerAngles += movement;
+        }
+        
+    }
+}
--- Assets/Vector3Script.cs
+++ Assets/Vector3Script.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Vector3Script : MonoBehaviour
 {
     public Vector3 myVector3;
+    public GameObject myObject;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myObject.transform.eulerAngles = myVector3;
     }
 
     // Update is called once per frame
